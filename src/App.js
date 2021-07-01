83e84982fcf83296774a2f419c17c61220f9b006
rucpata <rucinska.patrycja@gmail.com>
2021-07-01 15:51:59 +0200

    swich

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Route path='/:type?'>
-        <HomePage />
-      </Route>
-      
+      <Switch>
+        <Route path='/:type/:id'>
+          <PetDetailsPage/>
+        </Route>
+        <Route path='/:type?'>
+          <HomePage />
+        </Route>
+      </Switch>      
     </Router>
   );
 }
